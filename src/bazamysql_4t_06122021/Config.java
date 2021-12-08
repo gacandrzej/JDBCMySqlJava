0d2683d21f4242eaf68ff3f1ceadb7942bc1fb47
Andrzej Gac <gac.andrzej@gmail.com>
2021-12-08 16:19:40 +0100

    jdbc java

---
+++ src/bazamysql_4t_06122021/Config.java
@@ -0,0 +1,13 @@
+package bazamysql_4t_06122021;
+
+/**
+ * Zsmeie W Toruniu, last update 08 gru 2021r.
+ * @author Andrzej Gac <andrzej.gac@zsmeie.torun.pl>
+ */
+public class Config {
+public static int sizeX = 800;
+public static int sizeY = 600;
+    public Config() {
+    }
+    
+}
