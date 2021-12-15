d8bbd1dad813bedf1e0b34afe0366a77e6a92279
Andrzej Gac <gac.andrzej@gmail.com>
2021-12-15 16:07:15 +0100

    jdbc java

--- src/bazamysql_4t_06122021/OperacjeNaBazie.java
+++ src/bazamysql_4t_06122021/OperacjeNaBazie.java
@@ -2,13 +2,15 @@ package bazamysql_4t_06122021;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 /*
-* zadanie domowe termin długi
+* zadanie domowe termin długi 15 styczeń 2022r.max do 03 luty 2022r.
 * 0) rozmieszczenie elementów, ręcznie c.setLayout(new GroupGridLayout());
 * 1) okno logowania do aplikacji z autoryzacją w bazie
 * 2) czy w netbeansach możemy łączyć się po ssl lub tls?
@@ -21,13 +23,14 @@ To będzie na lekcjach:
 * 4) dodawanie danych
 */
 /**
- * Last update 08 gru 2021r.
+ * Last update 15 gru 2021r.
  * @author Andrzej Gac <andrzej.gac@zsmeie.torun.pl>
  */
 public class OperacjeNaBazie {
     Connection conn;
     ResultSet rs;
     Statement stmt;
+    PreparedStatement ptmt;
     String lp = "";
     String nazwa = "";
     String producent = "";
@@ -54,15 +57,66 @@ public class OperacjeNaBazie {
             }            
         } catch (SQLException ex) {
             Logger.getLogger(OperacjeNaBazie.class.getName()).log(Level.SEVERE, null, ex);
-        }finally{
-            try {
-                conn.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(OperacjeNaBazie.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        }finally{          
+            closeAll(rs, ptmt, stmt, conn);
         } 
         System.out.println("Wyniki:");
-        System.out.println(""+lp+" " +nazwa+" "+producent);
+        System.out.println(""+lp+" " +nazwa+" "+producent+" "
+                +data_sprzedazy+" "+cena+" "+waga);
+    }
+    
+    public boolean addRowToDatabase(){
+        
+        try {
+            conn = setConnection();
+            ptmt = (PreparedStatement) conn.prepareStatement(
+                    "insert into towary("
+                            + "nazwa,"
+                            + "producent,"
+                            + "data_sprzedazy,"
+                            + "cena,"
+                            + "waga) "
+                            + "values (?,?,?,?,?);");
+            ptmt.setString(1,  "klawiatura" );
+            ptmt.setString(2,  "Logitech" );
+            ptmt.setString(3,  "2021-12-13" );
+            ptmt.setString(4,  "99.99" );
+            ptmt.setString(5,   "0.45");
+                        
+            System.out.println("sql:"+ptmt);
+            ptmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(OperacjeNaBazie.class.getName()).log(Level.SEVERE, null, ex);
+        }finally {
+            closeAll(rs,ptmt,stmt,conn);         
+        }
+        return true;
+    }
+    public boolean findInDatabase(){
+        ArrayList<String> wynik = new ArrayList<>();
+        try {
+            conn = setConnection();
+            ptmt = conn.prepareStatement("select * from towary"
+                    + " where nazwa=? and producent=? ");
+            ptmt.setString(1, "klawiatura");
+            ptmt.setString(2, "Logitech");
+            
+            System.out.println("sql:"+ptmt);
+            rs = ptmt.executeQuery();
+            while(rs.next()) {
+                        wynik.add(rs.getString("nazwa"));
+                        wynik.add(rs.getString("producent"));
+                        wynik.add(rs.getString("data_sprzedazy"));
+                        wynik.add(rs.getString("cena"));
+                        wynik.add(rs.getString("waga"));
+          }
+            System.out.println("wyniki szukania: "+wynik);
+        } catch (SQLException ex) {
+            Logger.getLogger(OperacjeNaBazie.class.getName()).log(Level.SEVERE, null, ex);
+        }finally {
+            closeAll(rs, ptmt, stmt, conn);
+        }
+        return true;
     }
      public Connection setConnection() throws SQLException {
      
@@ -78,5 +132,39 @@ public class OperacjeNaBazie {
                 return conn;
     }
     return conn;
+    }
+
+    private void closeAll(ResultSet rs, PreparedStatement ptmt, Statement stmt, Connection conn) {
+       if (rs != null) 
+      
+                try {
+                    rs.close();
+       } catch (SQLException ex) {
+           Logger.getLogger(OperacjeNaBazie.class.getName()).log(Level.SEVERE, null, ex);
+       }
+           if (stmt != null)
+               
+               try {
+                   stmt.close();
+       } catch (SQLException ex) {
+           Logger.getLogger(OperacjeNaBazie.class.getName()).log(Level.SEVERE, null, ex);
+       }
+           
+            if (ptmt != null) 
+      
+                try {
+                    ptmt.close();
+       } catch (SQLException ex) {
+           Logger.getLogger(OperacjeNaBazie.class.getName()).log(Level.SEVERE, null, ex);
+       }
+         
+            if (conn != null) 
+      
+                try {
+                    conn.close();
+       } catch (SQLException ex) {
+           Logger.getLogger(OperacjeNaBazie.class.getName()).log(Level.SEVERE, null, ex);
+       };
+           
     }
 }
