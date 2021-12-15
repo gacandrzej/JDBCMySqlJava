e0a166a46580dd353b0c9b314221cb072095b6a9
Andrzej Gac <gac.andrzej@gmail.com>
2021-12-15 16:07:24 +0100

    jdbc java

--- src/bazamysql_4t_06122021/UserInterface.java
+++ src/bazamysql_4t_06122021/UserInterface.java
@@ -3,7 +3,7 @@ package bazamysql_4t_06122021;
 import javax.swing.JPanel;
 
 /**
- * Zsmeie W Toruniu, last update 08 gru 2021r.
+ * Zsmeie W Toruniu, last update 15 gru 2021r.
  * @author Andrzej Gac <andrzej.gac@zsmeie.torun.pl>
  */
 public class UserInterface extends JPanel{
@@ -11,6 +11,8 @@ public class UserInterface extends JPanel{
     public UserInterface() {
         OperacjeNaBazie o = new OperacjeNaBazie();
         o.selectAll();
+        //o.addRowToDatabase();
+        o.findInDatabase();
     }
     
 }
